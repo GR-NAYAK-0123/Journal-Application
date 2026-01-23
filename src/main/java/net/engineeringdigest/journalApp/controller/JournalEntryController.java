6e4dbd1b1abe0674652e28effbb4a4bedfa6bb72
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-01-23 08:50:39 +0530

    Added the user related API (save,delete,getAll,update)

--- src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -1,7 +1,9 @@
 package net.engineeringdigest.journalApp.controller;
 
 import net.engineeringdigest.journalApp.entity.JournalEntry;
+import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.service.JournalService;
+import net.engineeringdigest.journalApp.service.UserService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,20 +20,23 @@ import java.util.Optional;
 public class JournalEntryController {
     @Autowired
     private JournalService journalService;
+    @Autowired
+    private UserService userService;
 
-    @GetMapping
-    public ResponseEntity<?> getJournal(){
-        List<JournalEntry> list = journalService.getAllJournalEntry();
+    @GetMapping("{userName}")
+    public ResponseEntity<?> getJournalEntriesOfUser(@PathVariable String userName){
+        User user = userService.findByUserName(userName);
+        List<JournalEntry> list = user.getJournalEntries();
         if(list != null){
             return new ResponseEntity<>(list, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping
-    public ResponseEntity<?> postJournal(@RequestBody JournalEntry myJournal){
+    @PostMapping("{userName}")
+    public ResponseEntity<?> postJournal(@RequestBody JournalEntry myJournal, @PathVariable String userName){
         try{
-            journalService.saveJournalEntry(myJournal);
+            journalService.saveJournalEntry(myJournal, userName);
             return new ResponseEntity<>(myJournal, HttpStatus.CREATED);
         }
         catch(Exception e){
@@ -49,18 +53,18 @@ public class JournalEntryController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @DeleteMapping("id/{id}")
-    public ResponseEntity<?> deleteJournalById(@PathVariable ObjectId id){
-        journalService.deleteJournalEntryById(id);
+    @DeleteMapping("id/{userName}/{id}")
+    public ResponseEntity<?> deleteJournalById(@PathVariable String userName, @PathVariable ObjectId id){
+        journalService.deleteJournalEntryById(userName, id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PutMapping("id/{id}")
-    public ResponseEntity<?> updateJournalById(@PathVariable ObjectId id, @RequestBody JournalEntry newEntry){
+    @PutMapping("id/{userName}/{id}")
+    public ResponseEntity<?> updateJournalById(@PathVariable ObjectId id, @RequestBody JournalEntry newEntry, @PathVariable String userName){
         Optional<JournalEntry> oldEntry = journalService.getJournalEntryById(id);
-        if(oldEntry.get() != null){
+        if(oldEntry.isPresent()){
             oldEntry.get().setContent(newEntry.getContent() != null && !newEntry.getContent().isEmpty() ? newEntry.getContent() : oldEntry.get().getContent());
-            oldEntry.get().setTitle(newEntry.getTitle() != null && !newEntry.getTitle().isEmpty() ? newEntry.getTitle() : oldEntry.get().getTitle());
+            oldEntry.get().setTitle(!newEntry.getTitle().isEmpty() ? newEntry.getTitle() : oldEntry.get().getTitle());
             journalService.saveJournalEntry(oldEntry.get());
             return new ResponseEntity<>(oldEntry.get(),HttpStatus.OK);
         }
--- src/main/java/net/engineeringdigest/journalApp/service/JournalService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/JournalService.java
@@ -2,13 +2,13 @@ package net.engineeringdigest.journalApp.service;
 
 import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.entity.JournalEntry;
+import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.repository.IJournalRepo;
+import net.engineeringdigest.journalApp.repository.IUserRepo;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +18,10 @@ import java.util.Optional;
 public class JournalService {
     @Autowired
     private IJournalRepo journalRepo;
+    @Autowired
+    private IUserRepo userRepo;
+    @Autowired
+    private UserService userService;
 
     //This is the method who returns all the data from the database
     public List<JournalEntry> getAllJournalEntry(){
@@ -30,19 +34,28 @@ public class JournalService {
     }
 
     //This is a save method for saving the entity in the database
-    public void saveJournalEntry(JournalEntry newEntry){
+    public void saveJournalEntry(JournalEntry newEntry, String userName){
         try{
+            User user = userRepo.findByUserName(userName);
             newEntry.setDate(LocalDateTime.now());
-            journalRepo.save(newEntry);
+            JournalEntry saved = journalRepo.save(newEntry);
+            user.getJournalEntries().add(saved);
+            userRepo.save(user);
         }
         catch(Exception e){
             log.error("Exception", e);
         }
     }
 
+    public void saveJournalEntry(JournalEntry newEntry){
+        journalRepo.save(newEntry);
+    }
+
     //This is a method which is used to delete an entity from the database based on the id
-    public boolean deleteJournalEntryById(ObjectId id){
+    public void deleteJournalEntryById(String userName, ObjectId id){
+        User user = userRepo.findByUserName(userName);
+        user.getJournalEntries().removeIf(x -> x.getId().equals(id));
+        userService.saveUser(user);
         journalRepo.deleteById(id);
-        return true;
     }
 }
